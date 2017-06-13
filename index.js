b58d9a5c7292cc5480147002dde94eb3692b1633
Marco Solazzi <marco.solazzi+git@aquest.it>
2017-06-13 14:39:50 +0200

    multi files

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var colorPalette = require('./lib/colorPalette');
 
 module.exports = postcss.plugin('postcss-style-guide', function (opts) {
     opts = opts || {};
-    analyzer.setModules(syntaxHighlighter, markdownParser);
+    analyzer.setModules(opts.syntaxHighlighter || syntaxHighlighter, opts.markdownParser || markdownParser);
     var func = function (root, result) {
         var resultOpts = result.opts || {};
         try {
@@ -19,30 +19,33 @@ module.exports = postcss.plugin('postcss-style-guide', function (opts) {
         } catch (err) {
             throw err;
         }
-        var maps = analyzer.analyze(root, opts);
+        var maps = analyzer.analyze(root, opts, params);
         var palette = colorPalette.parse(root.toString());
         var promise = syntaxHighlighter.execute({
             src: params.src,
             tmplStyle: params.style,
             stylePath: require.resolve('highlight.js/styles/github.css')
-          }).then(function (styles) {
-            var html = template.rendering(maps, styles, {
-                project: params.project,
-                showCode: params.showCode,
-                tmpl: params.template,
-                colorPalette: palette
-            });
-            fileWriter.write(params.dest, html);
+        }).then(function (styles) {
+            maps.forEach((map) => {
+                var html = template.rendering({ root: map, list: maps }, styles, {
+                    project: params.project,
+                    showCode: params.showCode,
+                    tmpl: params.template,
+                    colorPalette: (map.id === 'index' ? palette : null)
+                });
+                const dest = path.join(path.dirname(params.dest), `${map.id}.html`);
+                fileWriter.write(dest, html);
 
-            if (!opts.silent) {
-                console.log('Successfully created style guide at ' + path.relative(process.cwd(), params.dest) + '!');
-            }
+                if (!opts.silent) {
+                    console.log('Successfully created style guide at ' + path.relative(process.cwd(), dest) + '!');
+                }
+            });
 
             return root;
-          }).catch(function (err) {
+        }).catch(function (err) {
             console.error('generate err:', err);
             return root;
-          });
+        });
         return promise;
     };
     return func;
